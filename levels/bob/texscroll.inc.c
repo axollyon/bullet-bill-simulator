6ef18059732c17998a7d11ba40e66b41c53b04d2
Axollyon <20480418+Axollyon@users.noreply.github.com>
2022-03-27 22:08:20 -0400

    grass n fog

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -1,6 +1,6 @@
 void scroll_bob_dl_Cube_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 60;
+	int count = 72;
 	int width = 64 * 0x20;
 	int height = 64 * 0x20;
 
@@ -20,6 +20,29 @@ void scroll_bob_dl_Cube_mesh_layer_1_vtx_0() {
 	currentY += deltaY;
 }
 
+void scroll_bob_dl_Cube_mesh_layer_1_vtx_1() {
+	int i = 0;
+	int count = 42;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bob_dl_Cube_mesh_layer_1_vtx_1);
+
+	deltaY = (int)(-3.200000047683716 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_bob() {
 	scroll_bob_dl_Cube_mesh_layer_1_vtx_0();
+	scroll_bob_dl_Cube_mesh_layer_1_vtx_1();
 }
--- levels/bob/texscroll.inc.h
+++ levels/bob/texscroll.inc.h
@@ -1,2 +1,3 @@
 extern void scroll_bob_dl_Cube_mesh_layer_1_vtx_0();
+extern void scroll_bob_dl_Cube_mesh_layer_1_vtx_1();
 extern void scroll_bob();
